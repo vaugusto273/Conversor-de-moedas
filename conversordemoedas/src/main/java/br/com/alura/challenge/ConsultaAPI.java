60e73ae093fe505fa311438971a0a537879d59a1
Vaugusto <samlara273@gmail.com>
2025-08-15 12:48:34 -0300

    ✨ Implement currency conversion functionality and user input handling

---
+++ conversordemoedas/src/main/java/br/com/alura/challenge/ConsultaAPI.java
@@ -0,0 +1,38 @@
+package br.com.alura.challenge;
+
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Properties;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class ConsultaAPI {
+    public static String moedaBase;
+    public static void setMoedaBase(String moeda) {
+        moedaBase = moeda;
+    }
+    public static String moedasDestino;
+    public static void setMoedasDestino(String moedasdeDestino) {
+        moedasDestino = moedasdeDestino;
+    }
+    public static void main(String[] args) throws Exception {
+        Properties props = new Properties();
+        try (var input = ConsultaAPI.class.getClassLoader().getResourceAsStream("config.properties")) {
+            props.load(input);
+        }
+        String apiKey = props.getProperty("API_KEY");
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(java.net.URI.create("https://v6.exchangerate-api.com/v6/"+ apiKey +"/latest/"+ moedaBase))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        String json = response.body();
+        JsonObject jsonObject = JsonParser.parseString(json).getAsJsonObject();
+        JsonObject rates = jsonObject.getAsJsonObject("conversion_rates");
+        double currencyValue = rates.get(moedasDestino).getAsDouble();
+        System.out.println("Taxas de conversão para " + moedasDestino + ":" + currencyValue);
+    }
+}
--- conversordemoedas/src/main/java/br/com/alura/challenge/Main.java
+++ conversordemoedas/src/main/java/br/com/alura/challenge/Main.java
@@ -1,7 +1,18 @@
 package br.com.alura.challenge;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite a moeda base (ex: USD, EUR, GBP): ");
+        ConsultaAPI.moedaBase = scanner.nextLine();
+        System.out.println("Digite a ou as moedas de destino (ex: BRL, JPY, AUD): ");
+        ConsultaAPI.moedasDestino = scanner.nextLine();
+        try {
+            ConsultaAPI.main(null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        scanner.close();
     }
 }
